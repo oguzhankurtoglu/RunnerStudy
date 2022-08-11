420c9ea078df778d44024f1b05cce032f9d93f4b
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2022-08-12 01:06:48 +0300

    add particle, character movement fix

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Script
@@ -7,14 +8,22 @@ namespace Script
     {
         public float playerSpeed;
         private Animator _animator;
-
+        public ParticleSystem confettie;
+        private PlatformManager _platformManager;
+        private float _playerXAxis;
         private Animator Animator => _animator ? _animator : GetComponentInChildren<Animator>();
 
+
+        private void Awake()
+        {
+            _platformManager = FindObjectOfType<PlatformManager>();
+        }
 
         private void Update()
         {
             playerSpeed = GameManager.Instance.gameState is GameState.Running or GameState.BeforeFinish ? 2 : 0;
-            
+            _playerXAxis = Mathf.Lerp(_playerXAxis, _platformManager.LastCube.transform.position.x, Time.deltaTime);
+            transform.position = new Vector3(_playerXAxis, transform.position.y, transform.position.z);
             Animator.SetFloat("Running", playerSpeed);
             Animator.SetFloat("Fall", transform.position.y);
             transform.Translate(Vector3.forward * (Time.deltaTime * playerSpeed));
@@ -24,8 +33,10 @@ namespace Script
         {
             if (other.CompareTag("Finish"))
             {
+                other.gameObject.SetActive(false);
                 GameManager.Instance.gameState = GameState.Success;
                 GetComponentInChildren<Animator>().SetTrigger("Dance");
+                confettie.Play();
             }
         }
     }
