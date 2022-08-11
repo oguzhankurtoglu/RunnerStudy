02e45f2b652641f9367eea08b88b0e9edc93c38a
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2022-08-11 18:07:46 +0300

    platform slice mechanic done

--- Assets/Script/PlatformItem.cs
+++ Assets/Script/PlatformItem.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Script
@@ -6,10 +8,21 @@ namespace Script
     public class PlatformItem : MonoBehaviour
     {
         public float speed;
+        public static PlatformItem CurrentCube { get; private set; }
+        public static PlatformItem LastCube { get; private set; }
 
         private void OnEnable()
         {
             EventManager.OnClickPressed.AddListener(Stop);
+            if (LastCube == null)
+            {
+                LastCube = GameObject.FindGameObjectWithTag("Base").GetComponent<PlatformItem>();
+            }
+            else
+            {
+                transform.localScale = LastCube.transform.localScale;
+                CurrentCube = this;
+            }
         }
 
         private void OnDisable()
@@ -17,19 +30,41 @@ namespace Script
             EventManager.OnClickPressed.RemoveListener(Stop);
         }
 
-        public void Stop()
+        private void Stop()
         {
             speed = 0;
+            float distance = transform.position.x - LastCube.transform.position.x;
+            float direction = distance > 0 ? 1f : -1f;
+            SlicePlatform(distance, direction);
+            LastCube = this;
         }
-        public void Move()
+
+        private void SlicePlatform(float distance, float direction)
         {
-            speed = 3.5f;
+            float sizeX = LastCube.transform.localScale.x - Mathf.Abs(distance);
+            float fallingSideSize = transform.localScale.x - sizeX;
+            float posX = LastCube.transform.position.x + (distance / 2);
+
+            transform.localScale = new Vector3(sizeX, transform.localScale.y, transform.localScale.z);
+            transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+
+            float cubeEdge = transform.position.x + (sizeX / 2 * direction);
+            float fallingSidePosition = cubeEdge + fallingSideSize / 2 *direction;
+
+            SpawnFallItem(fallingSidePosition, fallingSideSize);
+        }
+
+        private void SpawnFallItem(float fallingSidePosition, float fallingSideSize)
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(fallingSidePosition, transform.position.y, transform.position.z);
+            cube.transform.localScale = new Vector3(fallingSideSize, transform.localScale.y, transform.localScale.z);
+            cube.AddComponent<Rigidbody>().useGravity = true;
         }
 
         private void Update()
         {
             transform.position += Vector3.right * Time.deltaTime * speed;
         }
-        
     }
 }
--- Assets/Script/PlatformManager.cs
+++ Assets/Script/PlatformManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.GameObject;
+using Object = UnityEngine.Object;
 
 namespace Script
 {
@@ -13,113 +15,31 @@ namespace Script
 
         [SerializeField] private int forwardOffset = 3;
         [SerializeField] public List<GameObject> platformPool;
-        private Queue<GameObject> _platformQueue = new();
-        [SerializeField] private List<GameObject> slicedItemPool;
-        [SerializeField] private SlicerLeft slicerLeft;
-        [SerializeField] private SlicerRight slicerRight;
-        private GameObject _currentPlatform;
-
+        private readonly Queue<GameObject> _platformQueue = new();
 
         private Vector3 _defaultScale;
+        [SerializeField] private GameObject movingCubePrefab;
 
         #endregion
 
         #region UnityLifeCycle
 
-        private void OnEnable()
-        {
-            EventManager.OnClickPressed.AddListener(SetSlicersTransform);
-            EventManager.OnClickPressed.AddListener(TriggerPlatformItem);
-            PlatformProperty();
-        }
-
-        private void Start()
-        {
-            foreach (var platform in platformPool)
-            {
-                SetPoolItem(platform);
-            }
-        }
-
         private void Update()
         {
-            if (GameManager.Instance.gameState == GameState.Start ||
-                GameManager.Instance.gameState == GameState.Running)
+            if (GameManager.Instance.gameState == GameState.Running)
             {
                 if (Input.GetMouseButtonDown(0))
                 {
                     EventManager.OnClickPressed.Invoke();
-                    forwardOffset += 3;
                 }
             }
-        }
-
-        private void OnDisable()
-        {
-            EventManager.OnClickPressed.RemoveListener(SetSlicersTransform);
-            EventManager.OnClickPressed.RemoveListener(TriggerPlatformItem);
         }
 
         #endregion
 
         #region Methods
 
-        private void PlatformProperty()
-        {
-            var currentPlatform = platformPool[0];
-            _defaultScale = currentPlatform.transform.localScale;
-        }
-
-        private void SetSlicersTransform()
-        {
-            if (GameManager.Instance.gameState != GameState.Running) return;
-            slicerLeft.transform.SetParent(_currentPlatform.transform);
-            slicerLeft.transform.position =
-                new Vector3(_currentPlatform.transform.position.x - _currentPlatform.transform.localScale.x / 2,
-                    _currentPlatform.transform.position.y, forwardOffset);
-            
-            slicerRight.transform.SetParent(_currentPlatform.transform);
-            slicerRight.transform.position =
-                new Vector3(_currentPlatform.transform.position.x + _currentPlatform.transform.localScale.x / 2,
-                    _currentPlatform.transform.position.y, forwardOffset);
-        }
-
-        private void TriggerPlatformItem()
-        {
-            var currentItem = GetPoolItem();
-            _currentPlatform = currentItem.gameObject;
-            currentItem.transform.position += Vector3.forward * forwardOffset;
-            currentItem.Move();
-            StartCoroutine(ReturnPool(currentItem.gameObject));
-        }
-
-        private readonly WaitForSeconds _duration = new(6f);
-
-        private IEnumerator ReturnPool(GameObject returner)
-        {
-            yield return _duration;
-            SetPoolItem(returner);
-        }
-
-        private PlatformItem GetPoolItem()
-        {
-            var platformItem = _platformQueue.Dequeue();
-            platformItem.SetActive(true);
-            return platformItem.GetComponent<PlatformItem>();
-        }
-
-        private void SetPoolItem(GameObject poolItem)
-        {
-            ReturnDefault(poolItem);
-            _platformQueue.Enqueue(poolItem);
-        }
-
-        private void ReturnDefault(GameObject platformItem)
-        {
-            platformItem.transform.localScale = _defaultScale;
-            platformItem.transform.position = Vector3.right * -6;
-            platformItem.SetActive(false);
-        }
+   
 
         #endregion
     }
