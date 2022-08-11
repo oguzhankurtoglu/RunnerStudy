3ffb65ef4d0849c86e196b50dc53a4de5ed3bd27
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2022-08-11 21:19:47 +0300

    move platform item into platform manager

--- Assets/Script/PlatformItem.cs
+++ Assets/Script/PlatformItem.cs
@@ -8,60 +8,7 @@ namespace Script
     public class PlatformItem : MonoBehaviour
     {
         public float speed;
-        public static PlatformItem CurrentCube { get; private set; }
-        public static PlatformItem LastCube { get; private set; }
-
-        private void OnEnable()
-        {
-            EventManager.OnClickPressed.AddListener(Stop);
-            if (LastCube == null)
-            {
-                LastCube = GameObject.FindGameObjectWithTag("Base").GetComponent<PlatformItem>();
-            }
-            else
-            {
-                transform.localScale = LastCube.transform.localScale;
-                CurrentCube = this;
-            }
-        }
-
-        private void OnDisable()
-        {
-            EventManager.OnClickPressed.RemoveListener(Stop);
-        }
-
-        private void Stop()
-        {
-            speed = 0;
-            float distance = transform.position.x - LastCube.transform.position.x;
-            float direction = distance > 0 ? 1f : -1f;
-            SlicePlatform(distance, direction);
-            LastCube = this;
-        }
-
-        private void SlicePlatform(float distance, float direction)
-        {
-            float sizeX = LastCube.transform.localScale.x - Mathf.Abs(distance);
-            float fallingSideSize = transform.localScale.x - sizeX;
-            float posX = LastCube.transform.position.x + (distance / 2);
-
-            transform.localScale = new Vector3(sizeX, transform.localScale.y, transform.localScale.z);
-            transform.position = new Vector3(posX, transform.position.y, transform.position.z);
-
-            float cubeEdge = transform.position.x + (sizeX / 2 * direction);
-            float fallingSidePosition = cubeEdge + fallingSideSize / 2 *direction;
-
-            SpawnFallItem(fallingSidePosition, fallingSideSize);
-        }
-
-        private void SpawnFallItem(float fallingSidePosition, float fallingSideSize)
-        {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.position = new Vector3(fallingSidePosition, transform.position.y, transform.position.z);
-            cube.transform.localScale = new Vector3(fallingSideSize, transform.localScale.y, transform.localScale.z);
-            cube.AddComponent<Rigidbody>().useGravity = true;
-        }
-
+        
         private void Update()
         {
             transform.position += Vector3.right * Time.deltaTime * speed;
--- Assets/Script/PlatformManager.cs
+++ Assets/Script/PlatformManager.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 using static UnityEngine.GameObject;
 using Object = UnityEngine.Object;
 
@@ -11,18 +13,24 @@ namespace Script
 {
     public class PlatformManager : MonoBehaviour
     {
+        [field: SerializeField] public PlatformItem CurrentCube { get; set; }
+        [field: SerializeField] public PlatformItem LastCube { get; set; }
+
         #region fields
 
+        [SerializeField] public Transform spawnPoint;
         [SerializeField] private int forwardOffset = 3;
-        [SerializeField] public List<GameObject> platformPool;
-        private readonly Queue<GameObject> _platformQueue = new();
-
-        private Vector3 _defaultScale;
-        [SerializeField] private GameObject movingCubePrefab;
+        [SerializeField] private GameObject referencePrefab;
+        private Transform _defaultTransform;
 
         #endregion
 
         #region UnityLifeCycle
+
+        private void OnEnable()
+        {
+            _defaultTransform = referencePrefab.transform;
+        }
 
         private void Update()
         {
@@ -30,7 +38,9 @@ namespace Script
             {
                 if (Input.GetMouseButtonDown(0))
                 {
-                    EventManager.OnClickPressed.Invoke();
+                    Stop();
+                    forwardOffset += 3;
+                    SpawnCube();
                 }
             }
         }
@@ -39,7 +49,57 @@ namespace Script
 
         #region Methods
 
-   
+        private void SpawnCube()
+        {
+            var cube = CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = spawnPoint.transform.position + (Vector3.forward * forwardOffset);
+            cube.transform.localScale = LastCube.transform.localScale;
+            LastCube = CurrentCube;
+            CurrentCube = cube.GetComponent<PlatformItem>();
+        }
+
+        private void Stop()
+        {
+            LastCube.speed = 0;
+            float distance = CurrentCube.transform.position.x - LastCube.transform.position.x;
+            float direction = distance > 0 ? 1f : -1f;
+            if (Mathf.Abs(distance) >= LastCube.transform.localScale.x)
+            {
+                this.AddComponent<Rigidbody>();
+            }
+
+            SlicePlatform(distance, direction);
+        }
+
+        private void SlicePlatform(float distance, float direction)
+        {
+            float sizeX = LastCube.transform.localScale.x - Mathf.Abs(distance);
+            float fallingSideSize = CurrentCube.transform.localScale.x - sizeX;
+            float posX = LastCube.transform.position.x + (distance / 2);
+
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            cube.transform.localScale = new Vector3(sizeX, CurrentCube.transform.localScale.y,
+                CurrentCube.transform.localScale.z);
+            cube.transform.position =
+                new Vector3(posX, CurrentCube.transform.position.y, CurrentCube.transform.position.z);
+
+            float cubeEdge = CurrentCube.transform.position.x + (sizeX / 2 * direction);
+            float fallingSidePosition = cubeEdge + fallingSideSize / 2 * direction;
+
+            SpawnFallItem(fallingSidePosition, fallingSideSize);
+        }
+
+        private void SpawnFallItem(float fallingSidePosition, float fallingSideSize)
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(fallingSidePosition, CurrentCube.transform.position.y,
+                CurrentCube.transform.position.z);
+            cube.transform.localScale = new Vector3(fallingSideSize, CurrentCube.transform.localScale.y,
+                CurrentCube.transform.localScale.z);
+            cube.AddComponent<Rigidbody>().useGravity = true;
+            gameObject.SetActive(false);
+        }
 
         #endregion
     }
