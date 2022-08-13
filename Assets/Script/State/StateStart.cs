532c40b21e5568d60ea5dc9793a7bc41dd9402c3
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2022-08-13 19:14:44 +0300

    level loop fix

--- Assets/Script/State/StateStart.cs
+++ Assets/Script/State/StateStart.cs
@@ -17,12 +17,14 @@ namespace Script.State
             GameManager.player.platformManager.LastCube =
                 LevelManager.Instance.CurrentBase.GetComponent<PlatformItem>();
 
-            GameManager.player.platformManager.forwardOffset = (int) LevelManager.Instance.FinisPosition + 3;
+            GameManager.player.platformManager.forwardOffset =
+                (int) GameManager.player.platformManager.LastCube.transform.position.z + 3;
+            Debug.Log(GameManager.player.platformManager.forwardOffset);
             GameManager.player.MoveBase();
             yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
 
             GameManager.player.platformManager.Starter();
-            
+
 
             GameManager.SetState(new StateRunning(GameManager));
         }
